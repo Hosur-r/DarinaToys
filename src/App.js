94c494343dba68759c58759f0aa523b6308ebec1
hosur_ <skyebr@ynadex.ru>
2023-01-21 20:16:41 +0300

    header & entrances without working requests & working modalWin

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import Modal from "./components/modalWin/ModalWin";
-import { useState } from "react";
+import Header from "./components/header/Header";
 
 function App() {
 
-    const [isModal, setModal] = useState(true);
-    let content = 
-        <form className="modalForm">
-            <input placeholder="Введите логин" className="modalField" type="text"/>
-            <input placeholder="Введите e-mail" className="modalField" type="email"/>
-            <input placeholder="Введите пароль" className="modalField" type="password"/>
-            <input placeholder="Повторите пароль" className="modalField" type="password"/>
-            <button type="submit" className="modalBtn">Войти</button>
-        </form>
-
-    let footer = 
-    <p className="noAccount">
-      Нет аккаунта? <button className="noAccountBtn">Зарегистрироваться</button>
-    </p>
-
   return (
     <div className="App">
-
-           <Modal
-              isVisible={isModal}
-              title="Авторизация"
-              content={content}
-              footer={footer}
-              onClose={() => setModal(false)}
-            />
-
+        <Header/>
     </div>
   );
 }
