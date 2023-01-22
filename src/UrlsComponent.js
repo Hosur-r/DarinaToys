236419c3b4a6c30b193d98b7d33cf27d15968aa5
hosur_ <skyebr@ynadex.ru>
2023-01-22 13:43:06 +0300

    auth & reg & header

---
+++ src/UrlsComponent.js
@@ -0,0 +1,2 @@
+export const regUrl = "http://uni-team-inc.online:8080/register"
+export const authUrl = "http://uni-team-inc.online:8080/authorize"
--- src/components/entrances/requests/Requests.js
+++ src/components/entrances/requests/Requests.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 
-export const Register = async(name, email, psw, url) => {
-    await axios.post('http://uni-team-inc.online:8080/register', 
+export const Register = async(login, email, psw, url) => {
+    await axios.post(url, 
         JSON.stringify({
-          name: name,
+          name: login,
           email:email,
           password:psw
         })
@@ -17,15 +17,15 @@ export const Register = async(name, email, psw, url) => {
 
 
 
-export const Authorization = async(name, psw, url) => {
-    await axios.post('http://uni-team-inc.online:8080//authorize', 
+export const Authorization = async(login, psw, url) => {
+    await axios.post(url, 
         JSON.stringify({
-          name:name,
+          name:login,
           password:psw
         })
       )
       .then((data) => {
-        localStorage.setItem("access", data.data.access)
-        localStorage.setItem("refresh", data.data.refresh)
+        localStorage.setItem('access', data.data.access)
+        localStorage.setItem('refresh', data.data.refresh)
       })
 }
